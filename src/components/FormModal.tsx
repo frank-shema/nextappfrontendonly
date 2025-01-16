9879ccb226f1a4e55605667eb1bebbfd5a76d238
frank-shema <shemafranko0987@gmail.com>
2025-01-16 21:19:02 +0200

    made some changes

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -49,10 +49,10 @@ const FormModal = ({
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor =
     type === "create"
-      ? "bg-lamaYellow"
+      ? "bg-GamaYellow"
       : type === "update"
-      ? "bg-lamaSky"
-      : "bg-lamaPurple";
+      ? "bg-GamaSky"
+      : "bg-GamaPurple";
 
   const [open, setOpen] = useState(false);
 
